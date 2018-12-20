7301a3943e1ef54674d325a448115d61712a7114
vintersol <>
2018-12-20 16:29:26 +0100

    Add Flyway migration

--- recipeservice/src/main/java/com/bluesocks/entity/Recipe.java
+++ recipeservice/src/main/java/com/bluesocks/entity/Recipe.java
@@ -9,6 +9,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.math.BigInteger;
 
 @Data
 @AllArgsConstructor
--- recipeservice/src/main/resources/db/setup.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE recipe(
-	id SERIAL,
-	title VARCHAR(256),
-	url VARCHAR(256),
-	image_url VARCHAR(256),
-	ingredients VARCHAR(1024),
-
-	PRIMARY KEY(id)
-);
